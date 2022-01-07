8a5b5513a2eeb6aa92dbe727b0cc1acf8d47de59
Illia <66638336+Hamidalion@users.noreply.github.com>
2022-01-07 19:55:54 +0300

    [Udemy UnitTests] add arch folder with a projects and First NUnit test PortParserTest

---
+++ src/Udemy_UnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_UnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
