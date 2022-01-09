f4983e29a8bff79a0c7f86b262851de5815c6eaa
Illia <66638336+Hamidalion@users.noreply.github.com>
2022-01-09 13:54:27 +0300

    [Udemy_UnitTests] Add ExeptionTest and refactor last one

---
+++ tests/Business.Test/SerialPortParserTest.cs
@@ -0,0 +1,33 @@
+﻿using Business;
+using NUnit.Framework;
+using System;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class SerialPortParserTest
+    {
+        [Test]
+        public void ParserPort_COM1_Should_Return1()
+        {
+            int result = SerialPortParser.ParsePort("COM1");
+
+            Assert.That(result, Is.EqualTo(1));
+            // older style in NUnit is
+            // Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void ParsePort_WrongPortName_Should_Returns_Exeption()
+        {
+            // Arrange
+            string portName = "POR1";
+
+            // Act
+            TestDelegate action = () => SerialPortParser.ParsePort(portName);
+
+            // Assert
+            Assert.Throws<FormatException>(action);
+        }
+    }
+}
